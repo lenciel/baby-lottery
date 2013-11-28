d398c765f23cd67496c6c7bb82a6e0c5780ee1f4
lenciel <lenciel@gmail.com>
2013-11-29 01:39:31 +0800

    update model for greeting

--- src/apps/greeting/models.py
+++ src/apps/greeting/models.py
@@ -3,14 +3,16 @@
 from django.db import models
 
 
-class LeaveMessage(models.Model):
-    name = models.CharField(verbose_name=u'姓名', max_length=255)
-    phone = models.CharField(verbose_name=u'电话', max_length=255)
-    message = models.CharField(verbose_name=u'留言', max_length=2000, null=True, blank=True)
-    interests = models.CharField(verbose_name=u'兴趣', max_length=255, null=True, blank=True)
+class Greeting(models.Model):
+    gender = models.PositiveIntegerField(verbose_name=u'投注项')
+    name = models.CharField(verbose_name=u'姓名', max_length=32)
+    email = models.CharField(verbose_name=u'email', max_length=32)
+    message = models.CharField(verbose_name=u'祝福消息', max_length=2000)
+    lottery_code = models.CharField(verbose_name=u'幸运码', max_length=32)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=u"创建日期")
 
     class Meta:
-        verbose_name = u'合同节点'
+        verbose_name = u'祝福'
 
     def __unicode__(self):
-        return "%s-%s" % (self.name, self.phone)
+        return "%s-%s" % (self.email, self.message)
