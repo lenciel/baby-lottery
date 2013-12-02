973a2b411e6fb631f27278e4852a436c11a57bde
lenciel <lenciel@gmail.com>
2013-12-03 02:07:50 +0800

    add robots.txt

--- src/apps/urls.py
+++ src/apps/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, url, include
 from django.conf import settings
 from django.contrib import admin
+from django.http import HttpResponse
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
+                       url(r'^robots\.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /", mimetype="text/plain")),
                        url(r'^$', 'views.home', name='home'),
                        url(r'^greeting/', include('greeting.urls', namespace='greeting')),
 )
